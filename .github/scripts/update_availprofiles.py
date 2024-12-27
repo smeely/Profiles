9dd1ca24050c467cf35eb9a3f7013f13d9edc1ea
William Hinshaw <61280994+Hinshee@users.noreply.github.com>
2024-12-27 17:10:53 +0000

    Script Update
    Accomodates the new profile.json layout

--- .github/scripts/update_availprofiles.py
+++ .github/scripts/update_availprofiles.py
@@ -10,37 +10,32 @@ def get_profiles_data():
         for file in files:
             if file == 'profile.json':
                 profile_path = os.path.join(root, file)
-                print(f"Found profiles.json at: {profile_path}")
                 with open(profile_path) as f:
                     data = json.load(f)
+                    configurations = data.get("Configurations", [])
+                    for config in configurations:
+                        for file in config.get("Files", []):
+                            profile_file = os.path.join(root, file["FileName"])
+                            file["FileName"] = os.path.basename(profile_file)
                     profiles_data.append({
                         "Folder": os.path.basename(root),
                         "ProfileICAO": data.get("ProfileICAO", ""),
                         "Scenery": data.get("Scenery", ""),
                         "Creator": data.get("Creator", ""),
                         "Version": data.get("Version", 0),
-                        "Files": []
+                        "Configurations": configurations,
+                        "Changelog": data.get("Changelog", [])
                     })
     return profiles_data
 
-def update_avail_profiles(profiles_data):
-    for profile in profiles_data:
-        profile_folder = os.path.join(airports_dir, profile["Folder"])
-        for root, dirs, files in os.walk(profile_folder):
-            for file in files:
-                if file.endswith('.ini') or file.endswith('.py'):
-                    profile["Files"].append({"Name": file})
-    return profiles_data
-
 def main():
-    print("Starting to update AvailProfiles.json")
     profiles_data = get_profiles_data()
     if not profiles_data:
         print("No profile.json files found.")
-    avail_profiles = update_avail_profiles(profiles_data)
-    with open(avail_profiles_path, 'w') as f:
-        json.dump(avail_profiles, f, indent=4)
-    print("AvailProfiles.json has been updated.")
+    else:
+        with open(avail_profiles_path, 'w') as f:
+            json.dump(profiles_data, f, indent=4)
+        print("AvailProfiles.json has been updated.")
 
 if __name__ == '__main__':
     main()
