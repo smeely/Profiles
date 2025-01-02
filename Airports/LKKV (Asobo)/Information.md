6badf1d964f384ddb40d6a6c921c6f391ced5575
Jan Podlipsky <podlipskyjan@gmail.com>
2025-01-02 12:43:29 +0100

    Initial Release

---
+++ Airports/LKKV (Asobo)/Information.md
@@ -0,0 +1,6 @@
+Available from these retailers: MSFS Default Airport
+## Features:
+- Custom passenger walk-in paths
+- Custom vehicle placements
+- Custom pushback routes
+- Custom stand groups and stand names
---
+++ Airports/LKKV (Asobo)/lkkv-darriancze.py
@@ -0,0 +1,27 @@
+# -- coding: utf-8 --
+msfs_mode = 1
+icao = "lkkv"
+version = 1.0
+
+ApronM = CustomizedName("Apron M (Airliners) | Stand #§",1)
+ApronGA = CustomizedName("General Aviation | Stand #§",2)
+
+parkings = {
+    0 : {
+        None : ( ),
+        0 : (ApronGA, ),
+        1 : (ApronM, ),
+        2 : (ApronM, ),
+        3 : (ApronM, ),
+        4 : (ApronM, ),
+        11 : (ApronGA, ),
+        12 : (ApronGA, ),
+        13 : (ApronGA, ),
+        14 : (ApronGA, ),
+        15 : (ApronGA, ),
+        16 : (ApronGA, ),
+        17 : (ApronGA, ),
+        18 : (ApronGA, ),
+        19 : (ApronGA, )
+    }
+}
