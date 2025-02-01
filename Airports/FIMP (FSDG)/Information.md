22d542d94473da4747e8709c5de63640f3fb10b4
Kuba Barabasz <barabasz.kuba@gmail.com>
2025-02-01 15:34:31 +0000

    Initial Release

---
+++ Airports/FIMP (FSDG)/Information.md
@@ -0,0 +1,15 @@
+<!--- Licensed Under: CC BY-NC-ND 4.0 --->
+GSX profile FSDG's [FIMP Mauritius](https://fsdg-online.com/Sceneries/MSFS/MSFS-Full/Mauritius-MSFS::60.html) scenery
+
+## Features:
+- Custom vehicle placement at all gates
+- Custom pushbacks
+- Python file to group the gates together
+- Custom jetway heights to prevent pax from clipping through the jetways
+- GSX VDGS at gates 12-16
+- Ground Handling by Air Mauritius
+- Catering by LSG, Newrest and Servair
+- Accurate stop positions based on aircraft type
+- Gates 2-3 are configured for the ATR only
+
+Always use GSX's "reposition at this gate" feature when starting a flight
---
+++ Airports/FIMP (FSDG)/fimp-fsdg.py
@@ -0,0 +1,72 @@
+# -- coding: utf-8 --
+# Version: 1.1.0
+# Scenery = FSDG - FIMP Mauritius
+
+msfs_mode = 1
+icao = "fimp"
+
+terminal = CustomizedName("Terminal | Gate #§",1)
+ga_apron = CustomizedName("GA Apron | Parking #§",2)
+
+@AlternativeStopPositions
+def custom_offset_small(aircraftData):
+	distances = {
+		0: 0,
+		319: 2.8,
+		320: 2.8,
+		321: 2.8,
+		737: 2.8
+	}
+
+	return Distance.fromMeters(distances.get(aircraftData.idMajor, 0))
+
+@AlternativeStopPositions
+def custom_offset(aircraftData):
+	distances = {
+		0: 0,
+		300: 4.45,
+		310: 4.45,
+		319: 0,
+		320: 0,
+		321: 1.5,
+		737: 1.5,
+		787: 4.45,
+		330: 4.45,
+		340: 6,
+		767: 4.45,
+		747: 7.4
+	}
+
+	distances_777 = {
+		0: 7.4,
+		200: 7.4,
+		300: 10.3
+	}
+
+	if aircraftData.idMajor == 777:
+		return Distance.fromMeters(distances_777.get(aircraftData.idMinor, 0))
+	else:
+		return Distance.fromMeters(distances.get(aircraftData.idMajor, 0))
+
+
+parkings = {
+	GATE : {
+        None : (),
+        2 : (terminal, custom_offset_small),
+        3 : (terminal, ),
+        5 : (terminal, ),
+        7 : (terminal, ),
+        8 : (terminal, ),
+        9 : (terminal, ),
+        12 : (terminal, custom_offset),
+        13 : (terminal, custom_offset),
+        14 : (terminal, custom_offset),
+        16 : (terminal, custom_offset),
+        42 : (ga_apron, ),
+        43 : (ga_apron, ),
+        44 : (ga_apron, ),
+        45 : (ga_apron, ),
+        47 : (ga_apron, ),
+        48 : (ga_apron, )
+	}
+}
